8eee12e1027331abf719871cae5ce32b3a3d58dd
范峰博 <ffbnet@163.com>
2019-05-23 11:08:04 +0800

    netCore

---
+++ Sample1/Sample1/Controllers/ContentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample1.Models;
+
+namespace Sample1.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly List<Content> ContentList;
+        public ContentController(IOptionsSnapshot<List<Content>> option)
+        {
+            ContentList = option.Value;
+        }
+        public IActionResult Index()
+        {
+            return View(new ContentViewModel() { Contents = ContentList });
+            //var contents = new List<Content>();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            //}
+            //new Content().add_time.Humanize();
+            //return View(new ContentViewModel { Contents = contents });
+        }
+        public IActionResult TestLayout()
+        {
+            return View();
+        }
+
+    }
+}
